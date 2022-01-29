f9aa376efb62d4773443d6b1980bdbbf3d14d309
JAEL GIFTY <giftydomi72@gmail.com>
2022-01-29 18:23:00 +0000

    commit on main

--- gift.py
+++ gift.py
@@ -1,5 +1,51 @@
-import math
-lets do this 
-welcome back
-desktop
-computer
+# Python program to declare
+# empty list
+  
+# list is declared
+a = []         
+  
+print("Values of a:", a)
+print("Type of a:", type(a))
+print("Size of a:", len(a))    
+
+
+newlist1 = []
+
+type(newlist1)
+
+newlist2 = list()
+
+type(newlist2)
+
+nums = list(range(54,45,52))
+nums
+
+[54, 45.5, 52, 96, 87, 45, 78.5, 68, 45.3, 78, 98, 21, 65, 14, 52]
+
+
+# STRINGS
+name 1 = 'Ama'
+name 2 = 'Kojo'
+name 3 = 'Yhup'
+name 4 = 'Kwame'
+name 5 = 'Kwesi'
+
+type(name1)
+type(name2)
+
+print(firts_name)
+
+# INTERGERS
+num = 54
+num = 45.5
+num = 52
+num = 96
+num = 87
+num = 45 
+
+# BOOLEAN
+num = 45
+if num > 50:
+    print('Hello World')
+else:
+    print('Goodbye')
